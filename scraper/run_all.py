dca02141f3bde8178e4ecb21fb9a65c8eba54533
Rafa Diez <rafadiez@MacBook-Air-de-Rafa.local>
2026-03-10 11:46:26 -0600

    add mercadolibre scraper

--- scraper/run_all.py
+++ scraper/run_all.py
@@ -1,11 +1,18 @@
 from nike import scrape as scrape_nike
+from mercadolibre import scrape as scrape_ml
 from save_to_supabase import upsert_products
 
 def run():
+
     products = []
 
     print("Scraping Nike...")
-    products.extend(scrape_nike())
+    nike_products = scrape_nike()
+    products.extend(nike_products)
+
+    print("Scraping MercadoLibre...")
+    ml_products = scrape_ml()
+    products.extend(ml_products)
 
     print(f"Total productos recolectados: {len(products)}")
 
@@ -15,4 +22,3 @@ def run():
 
 
 if __name__ == "__main__":
-    run()
